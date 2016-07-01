29a728021dd55c7a6413cdb4096762cc0870d03e
Adam Georgsson <adamgeorgsson@gmail.com>
2016-07-01 15:16:31 +0000

    modified to live version

--- retweeter.py
+++ retweeter.py
@@ -9,21 +9,17 @@ auth.set_access_token(conf["ACCESS_KEY"], conf["ACCESS_SECRET"])
 api = tweepy.API(auth)
 
 userlist = Users()
-#while True:
-for user in userlist.users:
-	#print user.name
-	if user.lastID == 1:
-		try:	
-			for status in reversed(api.user_timeline(screen_name = user.name, since_id = user.lastID, count = 1, include_rts = True, page = 1)): #change include_rts
-				user.lastID = status.id
-				#print status.id
-				#api.retweet(status.id)
-		except tweepy.TweepError as e:
-			print user.name
-	    		print e.message[0]['code'], e.message[0]['message']
-	    		
-	    		
-	#time.sleep(1800) #sleep 30 min
-
-
-userlist.writeToFile("users.txt")
+while True:
+	for user in userlist.users:
+		#print user.name
+		if True: #user.lastID == 1:
+			try:	
+				for status in reversed(api.user_timeline(screen_name = user.name, since_id = user.lastID, count = 20, include_rts = False, page = 1)): #change include_rts
+					user.lastID = status.id
+					#print status.id
+					#api.retweet(status.id)
+			except tweepy.TweepError as e:
+				print user.name
+		    		print e.message[0]['code'], e.message[0]['message']
+	userlist.writeToFile("users.txt")
+	time.sleep(1800) #sleep 30 min
